3a61589c14f28d013d43ee0459e37fc8ef3cf93a
Dawn inShadow <dawninshadow0.0@gmail.com>
2017-12-09 13:17:42 +0800

    安装依赖

--- e-learning/src/main.js
+++ e-learning/src/main.js
@@ -1,15 +1,52 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import routes from './router'
+import Vuex from 'vuex'
+import Scrollactive from 'vue-scrollactive'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import iView from 'iview'
+import axios from 'axios'
+import 'iview/dist/styles/iview.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'sass/element-variables.sass'
+import store from './vuex/'
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+axios.defaults.baseURL = 'http://rap2api.taobao.org/app/mock/1162/'
+Vue.prototype.$axios = axios
+Vue.use(VueValidator)
+Vue.use(Vuex)
+Vue.use(ElementUI)
+Vue.use(Scrollactive)
+Vue.use(VueRouter)
+Vue.use(iView)
+const router = new VueRouter({
+    routes
 })
+router.beforeEach((to, from, next) => {
+        if (to.meta.requireAuth) {
+            if (store.state.userdata.role === 0) {
+                console.log(store.state.userdata.role)
+                console.log('验证失败')
+                next({
+                    path: '/basic/loginbasic/login',
+                    query: {
+                        redirect: to.fullPath
+                    }
+                })
+            } else {
+                console.log(store.state.userdata.role)
+                console.log('验证成功')
+                next()
+            }
+        } else {
+            next()
+        }
+    })
+    /* eslint-disable no-new */
+new Vue({
+    router, // router这个名字不能改
+    store
+    // render: h => h(App)
+}).$mount('#app')
