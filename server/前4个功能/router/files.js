a2bad5b1e43f07ca7f28b7c2a8559449fbb14bf6
Riunshow <zhubotaigg@gmail.com>
2017-12-21 21:15:33 +0800

    完成查询全部文件与单个文件

--- server/前4个功能/router/files.js
+++ server/前4个功能/router/files.js
@@ -5,6 +5,7 @@ const upload = multer({ dest: './uploads/' })
 const path = require('path');
 const fs = require('fs');
 const FileModel = require('../mongo/model/fileModel')
+const UserDataModel = require('../mongo/model/userModel')
 
 /**
  * 多文件，多种类上传
@@ -13,7 +14,7 @@ const FileModel = require('../mongo/model/fileModel')
 const cpUpload = upload.fields([{ name: 'test-upload', maxCount: 5 }, { name: 'gallery', maxCount: 8 }])
 
 
-// 上传接口
+// 文件上传
 router.post('/upload', cpUpload, (req, res) => {
     /**
      * 先判断是否登录
@@ -68,7 +69,8 @@ router.post('/upload', cpUpload, (req, res) => {
 
                     // 重命名文件
                     let oldPath = path.join(__dirname, "./../" + uploadFile.path);
-                    let newPath = path.join(__dirname, './../uploads/' + uploadFile.originalname);
+                    let newName = new Date().getTime() + uploadFile.originalname
+                    let newPath = path.join(__dirname, './../uploads/' + newName);
 
                     // 重命名
                     fs.rename(oldPath, newPath, (err) => {
@@ -91,7 +93,7 @@ router.post('/upload', cpUpload, (req, res) => {
                             // 创建 model
                             let _FileData = new FileModel({
                                 from: req.session.userInfo.userId,
-                                fileName: new Date().getTime() + uploadFile.originalname,
+                                fileName: newName,
                                 filePath: newPath,
                                 fileSize: (uploadFile.size / 1024).toFixed(2) + 'KB'
                             });
@@ -105,11 +107,16 @@ router.post('/upload', cpUpload, (req, res) => {
                                         msg: 'Something error!'
                                     })
                                 }
-                                console.log(results);
+                                /**
+                                 * fileId    文件 ID
+                                 * fileName  文件名字
+                                 * fromId    上传用户 ID
+                                 */
                                 res.send({
                                     code: 1,
-                                    name: uploadFile.fieldname,
-                                    path: newPath,
+                                    fileId: results._id,
+                                    fileName: uploadFile.fieldname,
+                                    fromId: results.from,
                                     msg: "上传成功"
                                 });
                             })
@@ -121,9 +128,89 @@ router.post('/upload', cpUpload, (req, res) => {
     }
 });
 
+// 查找所有已上传文件
+router.get('/alldata', (req, res) => {
+
+    // 数据库查询所有已存在文件
+    FileModel.find({}, (err, results) => {
+        if (err) {
+            console.log(err)
+            res.send({
+                code: -1,
+                msg: '文件数据查询失败'
+            })
+        }
+        if (results === null) {
+            res.send({
+                code: -2,
+                msg: '文件无数据'
+            })
+        } else {
+            /**
+             * allFile 返回所有数据
+             */
+            res.send({
+                code: 1,
+                allFile: results
+            })
 
 
+        }
+    })
+})
 
+// 查找单个文件
+router.post('/detail', (req, res) => {
+    const { fileId } = req.body
+
+    // 查找数据库中单条文件数据
+    FileModel.findOne({ _id: fileId }, (err, fileResult) => {
+        if (err) {
+            console.log(err)
+            res.send({
+                code: -1,
+                msg: '文件查询失败'
+            })
+        }
+        if (fileResult === null) {
+            res.send({
+                code: -2,
+                msg: '文件无数据'
+            })
+        } else {
+            // 根据文件中上传用户 ID 查找用户信息
+            UserDataModel.findOne({ _id: fileResult.from }, (err, userResult) => {
+                if (err) {
+                    console.log(err)
+                    res.send({
+                        code: -1,
+                        msg: '用户查询失败'
+                    })
+                }
+                if (userResult === null) {
+                    res.send({
+                        code: -2,
+                        msg: '用户无数据'
+                    })
+                } else {
+                    /**
+                     * fromName   上传用户名字
+                     * fileName   文件名字
+                     * fileSize   文件大小
+                     * date       文件上传日期
+                     */
+                    res.send({
+                        code: 1,
+                        fromName: userResult.name,
+                        fileName: fileResult.fileName,
+                        fileSize: fileResult.fileSize,
+                        date: fileResult.meta.updateAt
+                    })
+                }
+            })
+        }
+    })
+})
 
 
 
